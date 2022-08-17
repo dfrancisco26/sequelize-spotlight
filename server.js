600aafe443ac6fd5eb40016dbad38862fd387bda
Julie Nisbet <jmnisbet@gmail.com>
2022-08-16 23:07:38 -0700

    remove old pg code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('./lib/app');
-const pool = require('./lib/utils/pool');
 
 const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
@@ -12,5 +11,4 @@ app.listen(PORT, () => {
 process.on('exit', () => {
   // eslint-disable-next-line no-console
   console.log('👋  Goodbye!');
-  pool.end();
 });
