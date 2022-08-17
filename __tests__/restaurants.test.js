bfa0c46c2566932060bf66c6ffd60539ab636e95
Julie Nisbet <jmnisbet@gmail.com>
2022-08-16 21:54:55 -0700

    fixing models and migrations

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -29,38 +29,42 @@ const registerAndLogin = async (userProps = {}) => {
 describe('restaurant routes', () => {
   beforeEach(async () => {
     await db.sequelize.sync({ force: true });
-    db.Restaurant.bulkCreate([
-      {
-        name: 'Andina Restaurant',
-        cuisine: 'Peruvian',
-        cost: 3,
-        image:
-          'https://media-cdn.tripadvisor.com/media/photo-o/07/f6/b4/aa/lunch.jpg',
-        address: '1314 NW Glisan St Ste A, Portland, OR 97209-2717',
-        open: '9:00',
-        close: '5:00',
-        description:
-          'Andina draws inspiration from the native culinary traditions of Peru, as well as from the contemporary cooking by some of Peru’s leading chefs. Owner Doris Rodriguez de Platt, collaborates with Andina’s kitchen team to develop a menu that celebrates her family’s Andean roots, interweaving the ideas and techniques of both Criollo and Novo-Andean cuisines. The kitchen sources native and local ingredients to create dishes at once traditionally rooted and personally inspired. Founded in 2003, Andina emerged as one of the region’s leading Peruvian restaurants and continues to generate wide appeal within the food communities in and around Portland, and much further afield',
-        website: 'http://www.andinarestaurant.com/',
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        name: 'Bamboo Sushi',
-        cuisine: 'Japanese',
-        cost: 5,
-        image:
-          'https://media-cdn.tripadvisor.com/media/photo-o/08/10/77/19/bamboo-sushi.jpg',
-        address: '304 SE 28th Ave, Portland, OR 97214-1808',
-        open: '9:00',
-        close: '5:00',
-        description:
-          'At Bamboo Sushi, we make every decision with the highest regard for this world and the people in it. From transparency in how we source our food to reducing our carbon footprint to enriching the communities we live in; our mission is to change the way people eat, inspiring a shift within the restaurant industry and beyond. Everything you eat has an impact — It’s time to know yours.',
-        website: 'http://bamboosushi.com/',
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
+    try {
+      await db.Restaurant.bulkCreate([
+        {
+          name: 'Andina Restaurant',
+          cuisine: 'Peruvian',
+          cost: 3,
+          image:
+            'https://media-cdn.tripadvisor.com/media/photo-o/07/f6/b4/aa/lunch.jpg',
+          address: '1314 NW Glisan St Ste A, Portland, OR 97209-2717',
+          open: '9:00',
+          close: '5:00',
+          description:
+            'Andina draws inspiration from the native culinary traditions of Peru, as well as from the contemporary cooking by some of Peru’s leading chefs. Owner Doris Rodriguez de Platt, collaborates with Andina’s kitchen team to develop a menu that celebrates her family’s Andean roots, interweaving the ideas and techniques of both Criollo and Novo-Andean cuisines. The kitchen sources native and local ingredients to create dishes at once traditionally rooted and personally inspired. Founded in 2003, Andina emerged as one of the region’s leading Peruvian restaurants and continues to generate wide appeal within the food communities in and around Portland, and much further afield',
+          website: 'http://www.andinarestaurant.com/',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Bamboo Sushi',
+          cuisine: 'Japanese',
+          cost: 5,
+          image:
+            'https://media-cdn.tripadvisor.com/media/photo-o/08/10/77/19/bamboo-sushi.jpg',
+          address: '304 SE 28th Ave, Portland, OR 97214-1808',
+          open: '9:00',
+          close: '5:00',
+          description:
+            'At Bamboo Sushi, we make every decision with the highest regard for this world and the people in it. From transparency in how we source our food to reducing our carbon footprint to enriching the communities we live in; our mission is to change the way people eat, inspiring a shift within the restaurant industry and beyond. Everything you eat has an impact — It’s time to know yours.',
+          website: 'http://bamboosushi.com/',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ]);
+    } catch (e) {
+      console.log(e);
+    }
   });
   afterAll(async () => {
     await db.sequelize.close();
